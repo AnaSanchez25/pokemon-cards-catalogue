33e88ae60f4467a8e4d1388af4e3488dc2ce65fb
Ana <ana.t.sanchez25@gmail.com>
2026-01-07 12:55:09 -0800

    My first commit: Initial setup

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./css/index.css";
+import App from "./App.tsx";
+import Menu from "./menu/menu.tsx";
+// import Layout from "./Layout/Layout.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <Menu />
+    <App />
+  </StrictMode>
+);
---
+++ src/menu/menu.tsx
@@ -0,0 +1,19 @@
+function Menu() {
+  return (
+    <>
+      <ul>
+        <li>
+          <a href="default.asp">Home</a>
+        </li>
+        <li>
+          <a href="news.asp">Catalog</a>
+        </li>
+        <li>
+          <a href="contact.asp">About</a>
+        </li>
+      </ul>
+    </>
+  );
+}
+
+export default Menu;
